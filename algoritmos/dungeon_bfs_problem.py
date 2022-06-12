c8614de8c29cb3d8870799901081971152702df8
JKAmadio <jkahvedjian@gmail.com>
2022-06-12 16:54:30 -0300

    export video from dungeon_problem

--- algoritmos/dungeon_bfs_problem.py
+++ algoritmos/dungeon_bfs_problem.py
@@ -1,5 +1,4 @@
 '''
-
 You are trapped in a 2D dungeon and need to find the quickest way out!
 The dungeon is composed of unit cubes which may or may not be filled
 with rock. It takes one minute to move one unit north, south, east, west.
@@ -22,12 +21,16 @@ Is an escape possible? If yes, how long will it take?
   -----------------------------
 
 '''
+import matplotlib.pyplot as plt
+import cv2
+import os
+
 maze = [
-    [0, 0, 0, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0, 1, 0],
-    [0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 0, 0, 0],
-    [1, 0, 1, 0, 0, 1, 0]
+    [0, 0, 0, -10, 0, 0, 0],
+    [0, -10, 0, 0, 0, -10, 0],
+    [0, -10, 0, 0, 0, 0, 0],
+    [0, 0, -10, -10, 0, 0, 0],
+    [-10, 0, -10, 0, 0, -10, 0]
 ]
 
 visited = [
@@ -42,22 +45,37 @@ queue = []
 move_count = 0
 nodes_left_in_layer = 1
 nodes_in_next_layer = 0
+image_list = []
 
 def base():
-    print_maze()
+    # print_maze()
     bfs_2d([0,0], [4,3])
+    record_maze_changes()
 
 def print_maze():
-    for i in range(len(maze)):
-        print(maze[i])
-    print('\n')
+    fig, ax = plt.subplots()
+    image = ax.imshow(maze, cmap='bone')
+    image_list.append([image])
+    plt.savefig(f'./dungeon_path/dungeon_{move_count}')
+    # plt.show(block=True)
+
+def record_maze_changes():
+    image_list = [img for img in os.listdir('./dungeon_path/') if img.startswith('dungeon_') and img.endswith('.png')]
+    image_list.sort()
+    frame = cv2.imread(f'./dungeon_path/{image_list[0]}')
+    height, width, layers = frame.shape
+    video = cv2.VideoWriter('./dungeon_path/dungeon_video.avi', 0, 3, (width, height))
+    for image in image_list:
+        video.write(cv2.imread(f'./dungeon_path/{image}'))
+    cv2.destroyAllWindows()
+    video.release()
 
 def bfs_2d(start, end):
     global nodes_left_in_layer
     global nodes_in_next_layer
     global move_count
     queue.insert(0, start)
-    maze[start[0]][start[1]] = 2
+    maze[start[0]][start[1]] = 10
     visited[start[0]][start[1]] = True
     while len(queue) > 0:
         current = queue.pop()
@@ -83,12 +101,12 @@ def explore_neighbours(row, column):
         # não é vizinho válido se for out of bound do array
         if (rr < 0 or rr >= len(maze) or (cc < 0 or cc >= len(maze[0]))): continue
         # não é vizinho válido se for pedra
-        if maze[rr][cc] == 1: continue
+        if maze[rr][cc] == -10: continue
         # não é vizinho válido se já foi visitado
         if visited[rr][cc] == True: continue
         queue.insert(0, [rr, cc])
         visited[rr][cc] = True
-        maze[rr][cc] = 2
+        maze[rr][cc] = move_count + 10
         nodes_in_next_layer = nodes_in_next_layer + 1
 
 base()
