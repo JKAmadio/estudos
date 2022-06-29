9945c1b4407d3e303d7911e47890b4d8a7545234
JKAmadio <jkahvedjian@gmail.com>
2022-06-29 20:39:12 -0300

    rotate and flip submatrix

--- algoritmos/submatrix_studies/main.py
+++ algoritmos/submatrix_studies/main.py
@@ -9,20 +9,38 @@ slice_matrix_results = []
 brute_force_colors = ['#fafa6e', '#c4ec74', '#92dc7e', '#64c987', '#39b48e', '#089f8f', '#00898a', '#08737f', '#215d6e', '#2a4858']
 slice_matrix_colors = ['#e1363a', '#d62e44', '#c9284d', '#bb2653', '#ab2658', '#9b275b', '#8a295c', '#792a5b', '#682a58', '#582a53']
 
-for submatrix_side_size in submatrix_dimensions:
-    brute_force_seconds = []
-    slice_matrix_seconds = []
-    submatrix = np.random.randint(0, 2, size=(submatrix_side_size, submatrix_side_size))
-    for matrix_side_size in matrix_dimensions:
-        matrix = np.random.randint(0, 2, size=(matrix_side_size, matrix_side_size))
-        brute_force_seconds.append(submatrix_frequency.timer_brute_force(matrix, submatrix))
-        slice_matrix_seconds.append(submatrix_frequency.timer_slice_matrix(matrix, submatrix))
-        print(f'matriz {matrix_side_size} x {matrix_side_size} finalizada')
-    brute_force_results.append(brute_force_seconds)
-    slice_matrix_results.append(slice_matrix_seconds)
+def get_time_spent_on_methods():
+    for submatrix_side_size in submatrix_dimensions:
+        brute_force_seconds = []
+        slice_matrix_seconds = []
+        submatrix = np.random.randint(0, 2, size=(submatrix_side_size, submatrix_side_size))
+        for matrix_side_size in matrix_dimensions:
+            matrix = np.random.randint(0, 2, size=(matrix_side_size, matrix_side_size))
+            brute_force_seconds.append(submatrix_frequency.timer_brute_force(matrix, submatrix))
+            slice_matrix_seconds.append(submatrix_frequency.timer_slice_matrix(matrix, submatrix))
+            print(f'matriz {matrix_side_size} x {matrix_side_size} finalizada')
+        brute_force_results.append(brute_force_seconds)
+        slice_matrix_results.append(slice_matrix_seconds)
 
-for i in range(len(brute_force_results)):
-    plt.plot(matrix_dimensions, brute_force_results[i], color=brute_force_colors[i])
-    plt.plot(matrix_dimensions, slice_matrix_results[i], color=slice_matrix_colors[i])
+    for i in range(len(brute_force_results)):
+        plt.plot(matrix_dimensions, brute_force_results[i], color=brute_force_colors[i])
+        plt.plot(matrix_dimensions, slice_matrix_results[i], color=slice_matrix_colors[i])
 
-plt.show()
+    plt.show()
+
+def get_frequency_submatrix():
+    submatrix = np.random.randint(0, 2, size=(3, 3))
+    matrix = np.random.randint(0, 2, size=(2000, 2000))
+    slice_matrix_frequency = submatrix_frequency.slice_matrix(matrix, submatrix)
+    print(submatrix)
+    print(slice_matrix_frequency)
+    print_grid(matrix)
+
+def print_grid(matrix):
+    fig, ax = plt.subplots()
+    image = ax.imshow(matrix, cmap='bone')
+    # plt.savefig(f'./matrix.png')
+    plt.show(block=True)
+
+get_frequency_submatrix()
+
--- algoritmos/submatrix_studies/submatrix_frequency.py
+++ algoritmos/submatrix_studies/submatrix_frequency.py
@@ -19,43 +19,21 @@ import numpy as np
 import time
 start_time = time.time()
 
-# matrix = np.array([
-#         [1, 0, 1, 0, 0, 1, 0],
-#         [0, 0, 0, 0, 0, 1, 1],
-#         [0, 0, 1, 0, 0, 1, 0],
-#         [0, 0, 1, 0, 0, 1, 1],
-#         [0, 0, 1, 0, 0, 1, 0]
-#     ])
-
-matrix = np.random.randint(0, 2, size=(4000,4000))
-
-submatrix = np.array([
-        [1, 0, 1, 1],
-        [1, 0, 1, 1],
-        [1, 0, 1, 1],
-        [1, 0, 1, 1],
+matrix = np.array([
+        [0, 1, 1, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 1, 1],
+        [1, 0, 1, 0, 0, 0, 0],
+        [1, 0, 1, 0, 1, 1, 1],
+        [1, 0, 1, 0, 0, 1, 0]
     ])
 
-def brute_force(matrix, submatrix):
-    matrix_frequency = 0
-    for matrix_row in range(matrix.shape[0] - submatrix.shape[0] + 1):
-        for matrix_column in range(matrix.shape[1] - submatrix.shape[1] + 1):
-            elements_equal = 0
-            for submatrix_row in range(submatrix.shape[0]):
-                for submatrix_column in range(submatrix.shape[1]):
-                    if matrix[matrix_row + submatrix_row][matrix_column + submatrix_column] == submatrix[submatrix_row][submatrix_column]:
-                        elements_equal += 1
-                    if elements_equal == submatrix.shape[0] * submatrix.shape[1]:
-                        matrix_frequency += 1
-    return matrix_frequency
+# matrix = np.random.randint(0, 2, size=(100,100))
 
-def slice_matrix(matrix, submatrix):
-    matrix_frequency = 0
-    for i in range(matrix.shape[0] - submatrix.shape[0] + 1):
-        for j in range(matrix.shape[1] - submatrix.shape[1] + 1):
-            matrix_slice = matrix[i : (i + submatrix.shape[0]), j : (j + submatrix.shape[1])]
-            matrix_frequency += np.all(matrix_slice == submatrix)
-    return matrix_frequency
+submatrix = np.array([
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 0, 1]
+    ])
 
 def set_matrix_unique_variants(submatrix):
     all_variants = []
@@ -77,6 +55,30 @@ def set_matrix_unique_variants(submatrix):
         if (counter == len(unique_variants)):
             unique_variants.append(variant)
     return unique_variants
+
+def brute_force(matrix, submatrix):
+    matrix_frequency = 0
+    for matrix_row in range(matrix.shape[0] - submatrix.shape[0] + 1):
+        for matrix_column in range(matrix.shape[1] - submatrix.shape[1] + 1):
+            elements_equal = 0
+            for submatrix_row in range(submatrix.shape[0]):
+                for submatrix_column in range(submatrix.shape[1]):
+                    if matrix[matrix_row + submatrix_row][matrix_column + submatrix_column] == submatrix[submatrix_row][submatrix_column]:
+                        elements_equal += 1
+                    if elements_equal == submatrix.shape[0] * submatrix.shape[1]:
+                        matrix_frequency += 1
+    return matrix_frequency
+
+def slice_matrix(matrix, submatrix):
+    matrix_frequency = 0
+    for i in range(matrix.shape[0] - submatrix.shape[0] + 1):
+        for j in range(matrix.shape[1] - submatrix.shape[1] + 1):
+            submatrix_variants = set_matrix_unique_variants(submatrix)
+            matrix_slice = matrix[i : (i + submatrix.shape[0]), j : (j + submatrix.shape[1])]
+            for variant in submatrix_variants:
+                matrix_frequency += np.all(matrix_slice == variant)
+    return matrix_frequency
+
 
 def timer_brute_force(matrix, submatrix):
     start_time = time.time() 
