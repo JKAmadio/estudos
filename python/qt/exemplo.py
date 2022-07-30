9d4f79854444d8bc38c8320d39c619026034cb26
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2022-07-29 23:06:43 -0300

    QT - encapsulamento da janela

--- python/qt/exemplo.py
+++ python/qt/exemplo.py
@@ -3,33 +3,39 @@ from PySide6.QtGui import QFont, QAction
 from PySide6.QtWidgets import (QApplication, QLabel, QWidget, QVBoxLayout,
                                QPushButton, QMainWindow)
 app = QApplication()
-window = QMainWindow() # criamos um widget que aceita funcionalidades de janelas
-base = QWidget() # criamos um widget base que será composto por vários widget
-layout = QVBoxLayout() # escolhemos o layout que desejamos utilizar para posicionar os widgets internos
 
-menu = window.menuBar()
-file_menu = menu.addMenu('Menu')
-action = QAction('Ação')
-edit_menu = menu.addMenu('Editar')
-edit_action = QAction('Outra ação')
-file_menu.addAction(action)
-edit_menu.addAction(edit_action)
+# encapsulando a janela para poder eventualmente reduzir a dimensão do arquivo
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        base = QWidget() # criamos um widget base que será composto por vários widget
+        layout = QVBoxLayout() # escolhemos o layout que desejamos utilizar para posicionar os widgets internos
 
-# estabelecemos um tamanho de fonte
-font = QFont()
-font.setPixelSize(80)
+        menu = self.menuBar()
+        file_menu = menu.addMenu('Menu')
+        action = QAction('Ação')
+        edit_menu = menu.addMenu('Editar')
+        edit_action = QAction('Outra ação')
+        file_menu.addAction(action)
+        edit_menu.addAction(edit_action)
 
-# criamos um widget de label (basicamente um campo de texto)
-label = QLabel('OIEEEE')
-label.setFont(font) # indicamos que queremos usar a fonte configurada anteriormente
-label.setAlignment(Qt.AlignCenter) # indicamento que queremos centralizar
-layout.addWidget(label) # inserimos o label no layout
+        # estabelecemos um tamanho de fonte
+        font = QFont()
+        font.setPixelSize(80)
 
-botao = QPushButton('Click me!')
-botao.setFont(font)
-layout.addWidget(botao) # inserimos o botão no layout
+        # criamos um widget de label (basicamente um campo de texto)
+        label = QLabel('OIEEEE')
+        label.setFont(font) # indicamos que queremos usar a fonte configurada anteriormente
+        label.setAlignment(Qt.AlignCenter) # indicamento que queremos centralizar
+        layout.addWidget(label) # inserimos o label no layout
 
-base.setLayout(layout) # indicamos qual o layout a ser utilizado pelo widget base
-window.setCentralWidget(base) # indicamos o widget principal dessa janela
+        botao = QPushButton('Click me!')
+        botao.setFont(font)
+        layout.addWidget(botao) # inserimos o botão no layout
+
+        base.setLayout(layout) # indicamos qual o layout a ser utilizado pelo widget base
+        self.setCentralWidget(base) # indicamos o widget principal dessa janela
+
+window = Window() # criamos um widget que aceita funcionalidades de janelas
 window.show() # renderizamos a janela
 app.exec()
