3f71870e4a36d46af4d2aa631b86b4620d25e92e
JKAmadio <jkahvedjian@gmail.com>
2023-02-24 19:10:47 -0300

    separating options and composition visibility

--- vue/options-vs-composition/src/App.vue
+++ vue/options-vs-composition/src/App.vue
@@ -1,13 +1,29 @@
 <script setup>
-import OptionsAPI from './components/OptionsAPI.vue'
-import CompositionAPI from './components/CompositionAPI.vue'
+  import { ref } from 'vue';
+  import OptionsAPI from './components/OptionsAPI.vue'
+  import CompositionAPI from './components/CompositionAPI.vue'
+
+  let visibleAPI = ref('options');
+
+  const changeVisibleAPI = (newAPI) => {
+    visibleAPI.value = newAPI;
+  }
+
 </script>
 
 <template>
   <main>
+    <header>
+      <button @click="changeVisibleAPI('options')">
+        Options
+      </button>
+      <button @click="changeVisibleAPI('composition')">
+        Composition
+      </button>
+    </header>
     <div>
-      <OptionsAPI/>
-      <CompositionAPI />
+      <OptionsAPI v-if="visibleAPI === 'options'"/>
+      <CompositionAPI  v-if="visibleAPI === 'composition'"/>
     </div>
   </main>
 </template>
